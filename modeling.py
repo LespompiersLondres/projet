ce809498cd4db789eb00d88abdd59bfb27fca028
LespompiersLondres <140436034+LespompiersLondres@users.noreply.github.com>
2023-11-21 19:14:43 +0100

    Update modeling.py
    Remplacer les classifiers par des regressors

--- modeling.py
+++ modeling.py
@@ -1,14 +1,13 @@
-#meteo midas_uk
-
-
 import numpy as np
 import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn import neighbors
 from sklearn import svm
 from sklearn import ensemble
+from sklearn.metrics import accuracy_score
+from sklearn.ensemble import RandomForestRegressor
 
-merged_df=pd.read_csv("/home/lva/Bureau/dataScientest/projet_pompiers_Londres/data/merged_inner.csv")
+merged_df=pd.read_csv("/home/user/Bureau/dataScientest/projet_pompiers_Londres/data/merged_inner.csv")
 
 
 
@@ -21,13 +20,17 @@ X_train, X_test, y_train, y_test = train_test_split(df_train, target, test_size=
 score_knn=[]
 
 for k in range(2, 41):
-	knn=neighbors.KNeighborsClassifier(n_neighbors=k)
+	knn=neighbors.KNeighborsRegressor(n_neighbors=k)
 	knn.fit(X_train, y_train)
 	score_knn.append(knn.score(X_test, y_test))
 	
 
 y_knn = knn.predict(X_test)
-	
+
+
+knn_score = knn.score(X_train, y_train)
+print("KNeighborsRegressor score:", knn_score) 
+#print(accuracy_score(y_test, y_knn))
 #############################################
 
 clf = svm.SVC(gamma=0.01,  kernel='poly')
@@ -46,18 +49,6 @@ y_svc=clf.predict(X_test)
 #    'max_features': "sqrt", "log2", None
 #    'min_samples_split': Nombres pairs allant de 2 à 30
 
-clf = ensemble.RandomForestClassifier(n_jobs=-1, random_state=321)
+#clf = ensemble.RandomForestClassifier(n_jobs=-1, random_state=321)
+clf = RandomForestRegressor(max_depth=2, random_state=0)
 clf.fit(X_train, y_train)
-
-y_randomForest = clf.predict(X_test)
-
-
-###############################################
-#Comparaison
-from sklearn.metrics import confusion_matrix
-
-
-pd.crosstab(y_test, y_knn, rownames=['Classe réelle'], colnames=['Classe prédite knn'])
-pd.crosstab(y_test, y_svc, rownames=['Classe réelle'], colnames=['Classe prédite svc'])
-pd.crosstab(y_test, y_randomForest, rownames=['Classe réelle'], colnames=['Classe prédite random forest'])
-
